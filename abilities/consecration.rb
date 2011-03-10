b12a30558c502a0fe8c2798ecff8261af8b914a7
Lelon <github@lelon.net>
2011-03-10 07:09:13 -0800

    ongoing

--- abilities/consecration.rb
+++ abilities/consecration.rb
@@ -1,25 +1,21 @@
-class Consecration
+class Consecration < Ability
   
   def initialize(player, mob)
-    @player = player
-    @mob = mob
+    super(player, mob)
 
     @remaining_dot_ticks = 0
-    @on_cooldown = false
   end
 
   def reset
-    clear_cooldown
+    super
     @remaining_dot_ticks = 0
-    @damage = 0
-    @damage_remaining = 0
   end
 
   def use
     # TODO confirm all mechanics of consecration
     dmg = 810
-    dmg += @player.calculated_attack_power * 0.26
-    dmg += @player.calculated_spell_power * 0.26
+    dmg += @player.calculated_attack_power * 0.27
+    dmg += @player.calculated_spell_power * 0.27
 
     dmg *= @player.magic_bonus_multiplier
     # TODO everything
